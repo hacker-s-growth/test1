388dbc23ec56b30e2ac96dbb049ad619a4d5096d
Valentino1994 <demua1994@gmail.com>
2022-04-06 16:15:17 +0900

    first

---
+++ Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by Geunil Park on 2022/04/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Guell!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Geunil Park on 2022/04/05.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
